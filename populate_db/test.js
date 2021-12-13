3eefb029e753b6b605ad8cc338fb4816d3724cdc
triton <remi.triton@epitech.eu>
2021-12-13 10:59:35 +0100

    o

--- populate_db/test.js
+++ populate_db/test.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const bcrypt = require('bcryptjs')
 const fs = require('fs').promises;
 require('dotenv').config();
@@ -6,74 +6,77 @@ require('dotenv').config();
 // convert users.csv file to JSON array
 const main = async () => {
   const uri = process.env.MONGODB_URI
-  const client = new MongoClient(uri, { 
-     useNewUrlParser: true,
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+console.log(uri)
+  await client.connect();
+  const database = client.db('Babel');
+  const wineCol = database.collection('wines');
+  // const user = database.collection('users');
 
-    await client.connect();
-    const database = client.db('Babel');
-    const wineCol = database.collection('wines');
-    const user = database.collection('users');
+  // const password = await new Promise((resolve, reject) => {
+  //   bcrypt.genSalt(10, (err, salt) => bcrypt.hash('admin', salt, (err, hash) => {
+  //     if (err) {
+  //       reject(err)
+  //       return;
+  //     }
+  //     resolve(hash)
+  //     return;
+  //   }))
+  // })
 
-  const password = await new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, (err, salt) => bcrypt.hash('admin', salt, (err, hash) => {
-      if (err) {
-        reject(err)
-        return;
+  // user.insertOne({
+  //   firstName: 'admin',
+  //   lastName: 'admin',
+  //   email: 'admin@admin',
+  //   password,
+  //   role: "Admin",
+  // })
+
+
+  const fileContent = await fs.readFile('./populate_db/data.csv');
+
+  const fileContentStr = String(fileContent);
+
+  const data = fileContentStr
+    // each wine is on a new line
+    .split('\n')
+
+    // remove blank line
+    .filter((line) => !!line.trim())
+
+
+    // map string split by `;` to document
+    .map((line, i) => {
+      // escape first line
+      if (!i) {
+        return null;
       }
-      resolve(hash)
-      return;
-    }))
-  })
-
-    user.insertOne({
-      firstName: 'admin',
-      lastName: 'admin',
-      email: 'admin@admin',
-      password,
-      role: "Admin",
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      const [couleur, cuvee, domaine, departement, region, pays, millesime, cepage, vigneron, prix, quantite, description, type] = line.split(';')
+      return {
+        couleur: String(couleur.charAt(0).toUpperCase() + couleur.slice(1).toLowerCase() || ''),
+        domaine: String(domaine.charAt(0).toUpperCase() + domaine.slice(1).toLowerCase() || ''),
+        departement: String(departement.charAt(0).toUpperCase() + departement.slice(1).toLowerCase() ||  ''),
+        cuvee: String(cuvee.charAt(0).toUpperCase() + cuvee.slice(1).toLowerCase()) || '',
+        region: String(region.charAt(0).toUpperCase() + region.slice(1).toLowerCase() || ''),
+        pays: String(pays.charAt(0).toUpperCase() + pays.slice(1).toLowerCase()) || '',
+        millesime: String(millesime.charAt(0).toUpperCase() + millesime.slice(1).toLowerCase() || ''),
+        cepage: String(cepage.charAt(0).toUpperCase() + cepage.slice(1).toLowerCase()) ||  '',
+        vigneron: String(vigneron.charAt(0).toUpperCase() + vigneron.slice(1).toLowerCase() ||  ''),
+        prix: Number(prix ||  0),
+        quantite: Number(quantite || 0),
+        description,
+        type
+      };
     })
-
-
-    const fileContent = await fs.readFile('./populate_db/data.csv');
-
-    const fileContentStr = String(fileContent);
-
-    const data = fileContentStr
-        // each wine is on a new line
-        .split('\n')
-
-        // remove blank line
-        .filter((line) => !!line.trim())
-
-
-        // map string split by `;` to document
-        .map((line, i) => {
-            // escape first line
-            if (!i) {
-                return null;
-            }
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-            const [couleur, cuvee, domaine, region, pays, millesime, cepage, vigneron, prix, quantite] = line.split(';')
-            return {
-                couleur,
-                domaine,
-                cuvee,
-                region,
-                pays,
-                millesime,
-                cepage,
-                vigneron,
-                prix: Number(prix || 0),
-                quantite: Number(quantite || 0)
-            };
-        })
-        // remove NULL document (first line of the doc)
-        .filter(Boolean);
-    await wineCol.insertMany(data);
-    console.log('done');
-    process.exit(0);
+    // remove NULL document (first line of the doc)
+    .filter(Boolean);
+  await wineCol.insertMany(data);
+  console.log('done');
+  process.exit(0);
 }
 main();
 
